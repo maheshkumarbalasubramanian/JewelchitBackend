c26dca966eb13846a7ba78bee255398dc12f8adc
Maheshkumar <mahe31.tri@gmail.com>
2025-11-25 11:46:20 +0530

    Add ultra-simple ping endpoint for testing

---
+++ JewelChitApplication/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JewelChitApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
