30a256ab55495128379ab6b2f6826545cc857976
Maheshkumar <mahe31.tri@gmail.com>
2025-11-25 12:55:28 +0530

    Explicitly bind to PORT environment variable

--- JewelChitApplication/Program.cs
+++ JewelChitApplication/Program.cs
@@ -5,25 +5,27 @@ using JewelChitApplication.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAngular", policy =>
-    {
-        policy.WithOrigins(
-            "https://j-chit-frontend.vercel.app",
-            "http://localhost:4200"
-        )
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
-    });
-});
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAngular", policy =>
+//    {
+//        policy.WithOrigins(
+//            "https://j-chit-frontend.vercel.app",
+//            "http://localhost:4200"
+//        )
+//        .AllowAnyHeader()
+//        .AllowAnyMethod()
+//        .AllowCredentials();
+//    });
+//});
 
 var app = builder.Build();
 
-app.UseCors("AllowAngular");
+//app.UseCors("AllowAngular");
 app.MapControllers();
 
-Console.WriteLine("[INFO] Application starting...");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+Console.WriteLine($"[INFO] Starting on port {port}");
 Console.Out.Flush();
-app.Run();
+
+app.Run($"http://0.0.0.0:{port}");
