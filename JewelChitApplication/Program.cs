283a85c9426b074685fc3300fe6b4cd7cf6cdbc5
Maheshkumar <mahe31.tri@gmail.com>
2025-11-25 15:25:35 +0530

    Hceck

--- JewelChitApplication/Program.cs
+++ JewelChitApplication/Program.cs
@@ -1,12 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Force all logging to console
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.MapGet("/", () => "Hello World");
 app.MapControllers();
 
-Console.WriteLine("[INFO] Application building...");
-Console.Out.Flush();
-
-app.Run();
+try
+{
+    Console.WriteLine("[INFO] About to run app...");
+    System.Console.Out.Flush();
+    System.Console.Error.Flush();
+    app.Run();
+}
+catch (Exception ex)
+{
+    System.Console.Error.WriteLine($"[FATAL ERROR] {ex.Message}");
+    System.Console.Error.WriteLine($"[FATAL STACK] {ex.StackTrace}");
+    System.Console.Error.Flush();
+}
